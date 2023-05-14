003ea95d6ddf7f1d65344e0cefa723b1ead4976a
Bharath1910 <av.bharath@proton.me>
2023-05-14 10:39:36 +0530

    testing

---
+++ main.py
@@ -0,0 +1,178 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from dotenv import load_dotenv, find_dotenv
+from PIL import Image, ImageDraw, ImageFont
+import telebot
+import os
+
+
+import textwrap
+
+load_dotenv(find_dotenv())
+
+API_TOKEN = os.getenv("API_TOKEN")
+bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
+
+# {
+#     chatID: {
+#         userState: [],
+#         messages: []
+#     }
+# }
+userStates = {
+    1635159241: {
+        "userState": "1",
+        "messages": [
+            ["1", "hello he heheheheh"],
+            ["2", "hello bruh asdas hasdkjhb asashdja "],
+            ["3", "hello this isx 3 s"],
+            ["4", "hello"],
+            ["5", "hello"]
+        ]
+    }
+}
+
+dimensions = {
+    "1": {
+        "position": (23, 300),
+        "width": 10,
+        "lines": 3
+    },
+
+    "2": {
+        "position": (430, 50),
+        "width": 10,
+        "lines": 3
+    },
+
+    "3": {
+        "position": (13, 750),
+        "width": 10,
+        "lines": 3
+    },
+    "4": {
+        "position": (220, 600),
+        "width": 10,
+        "lines": 3
+    }
+}
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    chatID = message.chat.id
+    basePhoto = open('images/template.jpg', 'rb')
+    bot.send_photo(chatID, basePhoto)
+
+    markup = InlineKeyboardMarkup(row_width=2)
+    button1 = InlineKeyboardButton("1", callback_data="option1")
+    button2 = InlineKeyboardButton("2", callback_data="option2")
+    button3 = InlineKeyboardButton("3", callback_data="option3")
+    button4 = InlineKeyboardButton("4", callback_data="option4")
+    button5 = InlineKeyboardButton("5", callback_data="option5")
+
+    markup.add(button1, button2, button3, button4, button5)
+
+    bot.send_message(
+        chatID,
+        "Choose the number to edit", 
+        reply_markup=markup
+    )
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call):
+    chatID = call.message.chat.id
+    try: 
+        userStates[chatID]
+    
+    except KeyError:
+        userStates[chatID] = {}
+
+    if call.data == "option1":
+        bot.send_message(chatID, "Send me the text for 1")
+        userStates[chatID]["userState"] = "1"
+
+    elif call.data == "option2":
+        bot.send_message(chatID, "Send me the text for 2")
+        userStates[chatID]["userState"] = "2"
+    
+    elif call.data == "option3":
+        bot.send_message(chatID, "Send me the text for 3")
+        userStates[chatID]["userState"] = "3"
+
+    elif call.data == "option4":
+        bot.send_message(chatID, "Send me the text for 4")
+        userStates[chatID]["userState"] = "4"
+    
+    elif call.data == "option5":
+        bot.send_message(chatID, "Send me the text for 5")
+        userStates[chatID]["userState"] = "5"
+
+def setMessage(message, option):
+    chatID = message.chat.id
+    text = message.text
+
+    try: 
+        userStates[chatID]["messages"]
+    
+    except KeyError:
+        userStates[chatID]["messages"] = []
+    
+    finally:
+        userStates[chatID]["messages"].append([option, text])
+
+def drawBackground(draw, text_position, text, font, border_size=4):
+    for i in range(-border_size, border_size+1):
+        for j in range(-border_size, border_size+1):
+            draw.text(
+                (text_position[0]+i, text_position[1]+j),
+                text, font=font, fill=(0,0,0)
+            )
+
+    draw.text(text_position, text, (255, 255, 255), font=font)
+
+def generateMeme(chatID):
+    img = Image.open("images/meme_one.jpg")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("Poppins-Black.ttf", 50)
+
+    for option, message in userStates[chatID]["messages"]:
+        if option != "3":
+            continue
+
+        text_position = dimensions[option]["position"]
+        wrapper = textwrap.TextWrapper(width=dimensions[option]["width"])
+        lines = wrapper.wrap(text=message)
+
+        for line in lines:
+            drawBackground(draw, text_position, line, font)
+            text_position = (text_position[0], text_position[1]+60)
+
+    # text_position = (23, 420)
+    # wrapper = textwrap.TextWrapper(width=10)
+    # lines = wrapper.wrap(text=text)
+
+    # for line in lines:
+    #     drawBackground(draw, text_position, line, font)
+    #     text_position = (text_position[0], text_position[1]+60)
+
+    return img
+
+@bot.message_handler(commands=['meme'])
+def send_meme(message):
+    chatID = message.chat.id
+    meme = generateMeme(chatID)
+    bot.send_photo(chatID, meme)
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    chatID = message.chat.id
+    if chatID not in userStates and len(userStates[chatID]) == 0:
+        return
+
+    setMessage(message, option=userStates[chatID]["userState"])
+    print(userStates)
+
+    if len(userStates[chatID]["messages"]) == 5:
+        pass
+
+
+bot.infinity_polling()
